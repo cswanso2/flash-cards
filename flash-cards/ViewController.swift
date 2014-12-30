8714f7bd39544a8a88427357d49bfab3d463fa36
Charles Swanson <cswanso2@illinois.edu>
2014-12-29 18:12:54 -0600

    Initial Commit

---
+++ flash-cards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  flash-cards
+//
+//  Created by Charlie Swanson on 12/29/14.
+//  Copyright (c) 2014 Charlie Swanson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
